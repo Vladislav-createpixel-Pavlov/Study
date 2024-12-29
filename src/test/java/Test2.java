17fdb731b0612fe257e369e8d240d37e1a474c5f
VPavlov <vladislavih_pavlov@mail.ru>
2024-12-29 22:19:07 +0300

    2

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -1,17 +1,26 @@
+import io.qameta.allure.Allure;
+import io.qameta.allure.Attachment;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+
+import java.io.File;
 
 public class Test2
 {
     @org.junit.jupiter.api.Test
     public void Test1() {
-        RestAssured
+        String response = RestAssured
                 .given()
                 .log().all()
                 .when()
                 .contentType(ContentType.JSON)
                 .get("http://85.192.34.140:8080/api/easy/carBrands")
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+                .extract()
+                .body()
+                .toString();
+        Allure.addAttachment("Результат", "text/plain", response);
+
     }
 }
