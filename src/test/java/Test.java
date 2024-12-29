ac3da76676ab8613a13ff51651b0902402c3e0dd
KillTheSorcery <83838004+Vladislav-createpixel-Pavlov@users.noreply.github.com>
2024-12-29 11:25:38 +0300

    Update Test.java

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -7,30 +7,38 @@ import io.restassured.specification.RequestSpecification;
 
 public class Test
 {
+//    @org.junit.jupiter.api.Test
+//    public void main() {
+//        RequestSpecification requestSpecification = RestAssured.given().filter(new AllureRestAssured());
+//        requestSpecification.log().all();
+//        requestSpecification.baseUri("https://restful-booker.herokuapp.com/");
+//        requestSpecification.basePath("booking");
+//
+//        requestSpecification.body("{\n" +
+//                "    \"firstname\" : \"Jim\",\n" +
+//                "    \"lastname\" : \"Brown\",\n" +
+//                "    \"totalprice\" : 111,\n" +
+//                "    \"depositpaid\" : true,\n" +
+//                "    \"bookingdates\" : {\n" +
+//                "        \"checkin\" : \"2018-01-01\",\n" +
+//                "        \"checkout\" : \"2019-01-01\"\n" +
+//                "    },\n" +
+//                "    \"additionalneeds\" : \"Breakfast\"\n" +
+//                "}");
+//        requestSpecification.contentType(ContentType.JSON);
+//
+//        Response response = requestSpecification.post();
+//        ValidatableResponse validatableResponse = response.then();
+//        validatableResponse.statusCode(200);
+//    }
     @org.junit.jupiter.api.Test
-    public void main() {
-        RequestSpecification requestSpecification = RestAssured.given().filter(new AllureRestAssured());
-        requestSpecification.log().all();
-        requestSpecification.baseUri("https://restful-booker.herokuapp.com/");
-        requestSpecification.basePath("booking");
-
-        requestSpecification.body("{\n" +
-                "    \"firstname\" : \"Jim\",\n" +
-                "    \"lastname\" : \"Brown\",\n" +
-                "    \"totalprice\" : 111,\n" +
-                "    \"depositpaid\" : true,\n" +
-                "    \"bookingdates\" : {\n" +
-                "        \"checkin\" : \"2018-01-01\",\n" +
-                "        \"checkout\" : \"2019-01-01\"\n" +
-                "    },\n" +
-                "    \"additionalneeds\" : \"Breakfast\"\n" +
-                "}");
-        requestSpecification.contentType(ContentType.JSON);
-
-        Response response = requestSpecification.post();
-        ValidatableResponse validatableResponse = response.then();
-        validatableResponse.statusCode(200);
-
+    public void empyTest() {
+        RestAssured
+                .given()
+                .log().all()
+                .when()
+                .contentType(ContentType.JSON)
+                .then();
     }
 
 }
